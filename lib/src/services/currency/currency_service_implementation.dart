1edda46b6f2ef7c7bf58199b0dead32e62d80448
Ata Mahmoud <ata.ata250@hotmail.com>
2021-08-28 21:05:13 +0200

    Add CurrencyService and it's implementation class
    implement 'CurrencyService' to fecth exchange rates from cache or wep api

---
+++ lib/src/services/currency/currency_service_implementation.dart
@@ -0,0 +1,41 @@
+import '../../business_logic/models/rate.dart';
+import './currency_service.dart';
+import '../dependency_assembler.dart';
+import '../storage/storage_service.dart';
+import '../web_api/web_api.dart';
+
+class CurrencyServiceImplementation implements CurrencyService {
+  final _storageService = dependencyAssmbler<StorageService>();
+  final _webApiService = dependencyAssmbler<WebApi>();
+
+  @override
+  Future<Rate> getExchangeRate() async {
+    bool isValidCache = await _isValidCachedRate();
+
+    final cachedRate = await _storageService.getExchangeRate();
+    if (isValidCache) {
+      return cachedRate!;
+    }
+
+    try {
+      final rate = await _webApiService.fetchExchangeRate();
+      _storageService.saveExchangeRate(rate);
+      return rate;
+    } catch (e) {
+      if (cachedRate == null) rethrow;
+      return cachedRate;
+    }
+  }
+
+  Future<bool> _isValidCachedRate() async {
+    const int _fetchTimePeriodInMinutes = 1;
+    final rate = await _storageService.getExchangeRate();
+
+    if (rate == null) return false;
+
+    final rateFetchTime = DateTime.fromMillisecondsSinceEpoch(rate.fetchTime);
+    Duration difference = DateTime.now().difference(rateFetchTime);
+
+    return difference.inMinutes < _fetchTimePeriodInMinutes;
+  }
+}

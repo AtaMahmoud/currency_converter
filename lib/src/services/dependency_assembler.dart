bfbdb8df2f0c32b33a31457c31054d23c7e98ea8
Ata Mahmoud <ata.ata250@hotmail.com>
2021-08-28 22:13:38 +0200

    Register CurrencyService at dependencyAssmbler

--- lib/src/services/dependency_assembler.dart
+++ lib/src/services/dependency_assembler.dart
@@ -4,6 +4,8 @@ import './storage/storage_service.dart';
 import './storage/shared_prefernces_storage.dart';
 import './web_api/coingecko_web_api.dart';
 import './web_api/web_api.dart';
+import './currency/currency_service.dart';
+import './currency/currency_service_implementation.dart';
 
 GetIt dependencyAssmbler = GetIt.instance;
 
@@ -11,4 +13,6 @@ void setupDependencyAssembler() {
   dependencyAssmbler.registerFactory<WebApi>(() => CoingeckoWebApi());
   dependencyAssmbler
       .registerFactory<StorageService>(() => SharedPreferncesStorage());
+  dependencyAssmbler
+      .registerFactory<CurrencyService>(() => CurrencyServiceImplementation());
 }
